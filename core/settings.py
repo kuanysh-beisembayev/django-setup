eb98d12cd1dea10b2136cc482751bb67a581692d
kuanysh-beisembayev <kuanysh.beisembayev@zimran.io>
2023-05-17 21:05:27 +0600

    add dockerfile, docker-compose.yml

--- core/settings.py
+++ core/settings.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+    'default': env.db_url('DATABASE_URL'),
 }
 
 # Password validation
